8509e8dfe04d5121098eaee29026363e6fb2e96f
Seong-Hoon-Lim <hooney200@gmail.com>
2023-09-16 22:14:46 +0900

    #2 - 개발 환경 세팅
    * spring boot 프로젝트 생성
    * gradle 빌드 및 프로젝트 빌드 버전 맞춤 (개발 편의상 시큐리티 의존성은 주석처리 후 개발 진행)
    * application.yaml 세팅 (로그, DB 관련 세팅)
    * spring boot 애플리케이션 정상 동작 확인
    * .gitgnore 지정

---
+++ springboot-kittopmall-project/src/main/java/com/kittopmall/SpringbootKittopmallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kittopmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKittopmallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKittopmallProjectApplication.class, args);
+    }
+
+}
