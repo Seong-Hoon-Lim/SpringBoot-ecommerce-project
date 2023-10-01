1b98669ca8be3c3ebec3995f24072ef97831494e
Seong-Hoon-Lim <hooney200@gmail.com>
2023-10-01 23:33:42 +0900

    #14 - UserService 인터페이스에 UserVo 객체를 UserDto 객체로 수정

--- springboot-kittopmall-project/src/main/java/com/kittopmall/service/UserService.java
+++ springboot-kittopmall-project/src/main/java/com/kittopmall/service/UserService.java
@@ -1,23 +1,24 @@
 package com.kittopmall.service;
 
+import com.kittopmall.dto.UserDto;
 import com.kittopmall.vo.UserVo;
 
 public interface UserService {
 
     //회원 정보 등록
-    void registerUser(UserVo userVo);
+    void registerUser(UserDto userDto);
 
     //회원 아이디로 회원 조회
-    UserVo getUserByUserId(Long id);
+    UserDto getUserByUserId(Long id);
 
     //회원 이메일로 회원 조회
-    UserVo getUserByEmail(String email);
+    UserDto getUserByEmail(String email);
 
     //회원 닉네임으로 회원 조회
-    UserVo getUserByNickname(String nickname);
+    UserDto getUserByNickname(String nickname);
 
     //회원 정보 수정
-    void modifyUser(UserVo userVO);
+    void modifyUser(UserDto userDto);
 
     //회원 정보 삭제
     void removeUser(Long userId);
