2ebdee53eb6a395b81d383d7fdaea07817686d89
Seong-Hoon-Lim <hooney200@gmail.com>
2023-10-29 19:54:05 +0900

    #18 - 회원 관리 관련 뷰 컨트롤러 구현을 위한 테스트
    * GET 메소드만 테스트 진행
    * 회원 정보 페이지의 인증 없는 사용자  경우 signin 뷰로 이동하는 경우 외 테스트 시 404 코드 반환
    * 테스트 예상 값 확인 되므로 테스트 기반으로 컨트롤러 메소드 정의 및 view 페이지 생성 후 재 테스트 필요

---
+++ springboot-kittopmall-project/src/test/java/com/kittopmall/controller/UserControllerTest.java
@@ -0,0 +1,169 @@
+package com.kittopmall.controller;
+
+import com.kittopmall.config.SecurityConfig;
+import com.kittopmall.dto.UserDto;
+import com.kittopmall.mapper.UserMapper;
+import com.kittopmall.service.UserServiceImpl;
+import com.kittopmall.vo.UserVo;
+import com.kittopmall.vo.constants.Role;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.*;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("회원관리 controller 테스트")
+@Log4j2
+@Import(SecurityConfig.class)
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    private final MockMvc mvc;
+    @MockBean private UserServiceImpl userService;
+    @MockBean private UserMapper userMapper;
+    @MockBean private ModelMapper mapper;
+
+    public UserControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[View][GET] 회원가입 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingDisplayingSignupView_thenReturnSignupView() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/kittop/signup"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("user/signup"))
+                .andExpect(model().attributeExists("user"));
+    }
+
+    @DisplayName("[View][GET] 회원 로그인 페이지 - 정상 호출")
+    @WithMockUser
+    @Test
+    void givenNothing_whenRequestingDisplayingSigninView_thenReturnsSigninView() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/kittop/signin"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("user/signin"))
+                .andExpect(model().attributeExists("loginForm"));
+    }
+
+    @DisplayName("[View][GET] 회원 정보 페이지 - 인증이 없다면 로그인 페이지로 이동")
+    @Test
+    void givenNothing_whenRequestingDisplayingUserInfoView_thenRedirectsToSigninView() throws Exception {
+        //Given
+        long userId = 1L;
+
+        //When & Then
+        mvc.perform(get("/kittop/user/" + userId))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/signin"));
+        then(userService).shouldHaveNoInteractions();
+    }
+
+    @DisplayName("[View][GET] 회원 정보 페이지 / 회원 수정 페이지 - 정상 호출, 인증된 사용자")
+    @WithMockUser
+    @Test
+    void givenNothing_whenRequestingDisplayingUserInfoView_thenReturnsUserInfoView() throws Exception {
+        //Given
+        long userId = 1L;
+        UserDto dto = createUserDto();
+        given(mapper.map(any(UserVo.class), eq(UserDto.class))).willReturn(createUserDto());
+        given(userMapper.findUserByUserId(userId)).willReturn(createUser());
+
+        //When & Then
+        mvc.perform(get("/kittop/user/" + userId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("user/info"))
+                .andExpect(model().attributeExists("user"));
+        then(userService).should().getUserByUserId(userId);
+    }
+
+
+    private UserVo updatedUser() {
+        return UserVo.builder()
+                .email("modify@gmail.com")
+                .password("1111")
+                .nickname("modify")
+                .name("modify")
+                .birth("1990/01/01")
+                .gender("남")
+                .addr("34005/대전광역시 유성구 대덕대로1111번길 1-8/가나타운 1동 1호")
+                .phone("01012341111")
+                .provider(null)
+                .providerId(null)
+                .createdBy("modify")
+                .role(Role.ROLE_USER)
+                .build();
+    }
+
+    private UserDto updatedUserDto() {
+        return UserDto.builder()
+                .email("modify@gmail.com")
+                .password("1111")
+                .nickname("modify")
+                .name("modify")
+                .birth("1990/01/01")
+                .gender("남")
+                .addr("34005/대전광역시 유성구 대덕대로1111번길 1-8/가나타운 1동 1호")
+                .phone("01012341111")
+                .provider(null)
+                .providerId(null)
+                .createdBy("modify")
+                .role(Role.ROLE_USER)
+                .build();
+    }
+    private UserDto createUserDto() {
+        return UserDto.builder()
+                .email("test@gmail.com")
+                .password("1234")
+                .nickname("test")
+                .name("test")
+                .birth("1990/01/01")
+                .gender("남")
+                .addr("34005/대전광역시 유성구 대덕대로1111번길 1-8/가나타운 1동 1호")
+                .phone("01012341111")
+                .provider(null)
+                .providerId(null)
+                .createdBy("test")
+                .role(Role.ROLE_USER)
+                .build();
+    }
+
+    private UserVo createUser() {
+        return UserVo.builder()
+                .email("test@gmail.com")
+                .password("1234")
+                .nickname("test")
+                .name("test")
+                .birth("1990/01/01")
+                .gender("남")
+                .addr("34005/대전광역시 유성구 대덕대로1111번길 1-8/가나타운 1동 1호")
+                .phone("01012341111")
+                .provider(null)
+                .providerId(null)
+                .createdBy("test")
+                .role(Role.ROLE_USER)
+                .build();
+    }
+
+}
