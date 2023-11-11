bbb4082b6b26d18b688144e98f3360afba82e303
Seong-Hoon-Lim <hooney200@gmail.com>
2023-11-11 23:57:55 +0900

    #20 - SecurityConfig 에 JWT 사용을 위한 csrf 미사용 처리

--- springboot-kittopmall-project/src/main/java/com/kittopmall/config/SecurityConfig.java
+++ springboot-kittopmall-project/src/main/java/com/kittopmall/config/SecurityConfig.java
@@ -5,14 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity //스프링 시큐리티를 활성화하고 웹 보안 설정을 구성
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                //jwt 토큰 사용으로 불필요
+                .csrf().disable()
+
                 .authorizeRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .mvcMatchers(
